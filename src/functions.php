0aad1582717329953cb101a5ccc99a49f5f4d408
yangyi <yangyi.cn.gz@gmail.com>
2016-05-31 08:45:58 +0800

    代码调整

--- src/functions.php
+++ src/functions.php
@@ -1,10 +1,10 @@
 <?php
-namespace Owl\Service {
-    function get($id)
-    {
-        $args = func_get_args();
-        $container = \Owl\Service\Container::getInstance();
+namespace Owl\Service;
 
-        return call_user_func_array([$container, 'get'], $args);
-    }
+function get($id)
+{
+    $args = func_get_args();
+    $container = \Owl\Service\Container::getInstance();
+
+    return call_user_func_array([$container, 'get'], $args);
 }
