b61629e34cb5f36f884f025789b7a4bf719ce0fb
hsldymq <hsldymq@qq.com>
2021-04-13 00:25:36 +0800

    refactor: UUID Type

--- src/DataMapper/Type/UUID.php
+++ src/DataMapper/Type/UUID.php
@@ -2,6 +2,8 @@
 
 namespace Owl\DataMapper\Type;
 
+use Owl\Helpers\UUID as UUIDHelper;
+
 class UUID extends Common
 {
     public function normalizeAttribute(array $attribute)
@@ -32,22 +34,8 @@ class UUID extends Common
         return $uuid;
     }
 
-    // http://php.net/manual/en/function.uniqid.php#94959
-    public static function generate()
+    public static function generate(): string
     {
-        if (version_compare(PHP_VERSION, '7.0.0', '<')) {
-            return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-                mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-                mt_rand(0, 0x0fff) | 0x4000,
-                mt_rand(0, 0x3fff) | 0x8000,
-                mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-            );
-        } else {
-            $uuid = bin2hex(random_bytes(18));
-            $uuid[8] = $uuid[13] = $uuid[18] = $uuid[23] = '-';
-            $uuid[14] = '4';
-
-            return $uuid;
-        }
+        return UUIDHelper::generateV4();
     }
 }
