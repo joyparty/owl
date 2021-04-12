f2dbbd46776360b9dc44860e93b53952c05dc9b6
hsldymq <hsldymq@qq.com>
2021-04-13 00:59:52 +0800

    refactor: Hooks

--- src/DataMapper/Cache/Hooks.php
+++ src/DataMapper/Cache/Hooks.php
@@ -1,5 +1,9 @@
 <?php
+
 namespace Owl\DataMapper\Cache;
+
+use Owl\DataMapper\Data;
+use Owl\Service;
 
 /**
  * @example
@@ -65,9 +69,9 @@ trait Hooks
     /**
      * create cache after save new data, if cache policy set.
      *
-     * @param \Owl\DataMapper\Data $data
+     * @param Data $data
      */
-    protected function __afterInsert(\Owl\DataMapper\Data $data)
+    protected function __afterInsert(Data $data)
     {
         $policy = $this->getCachePolicy();
         $id = $data->id(true);
@@ -87,9 +91,9 @@ trait Hooks
     /**
      * delete or update cache after save data.
      *
-     * @param \Owl\DataMapper\Data $data
+     * @param Data $data
      */
-    protected function __afterUpdate(\Owl\DataMapper\Data $data)
+    protected function __afterUpdate(Data $data)
     {
         $policy = $this->getCachePolicy();
         $id = $data->id(true);
@@ -109,9 +113,9 @@ trait Hooks
     /**
      * delete cache after delete data.
      *
-     * @param \Owl\DataMapper\Data $data
+     * @param Data $data
      */
-    protected function __afterDelete(\Owl\DataMapper\Data $data)
+    protected function __afterDelete(Data $data)
     {
         $this->deleteCache($data->id(true));
 
@@ -121,11 +125,11 @@ trait Hooks
     /**
      * delete cache before refresh data.
      *
-     * @param \Owl\DataMapper\Data $data
+     * @param Data $data
      *
-     * @return \Owl\DataMapper\Data
+     * @return Data
      */
-    public function refresh(\Owl\DataMapper\Data $data)
+    public function refresh(Data $data)
     {
         $this->deleteCache($data->id(true));
 
@@ -135,9 +139,10 @@ trait Hooks
     /**
      * 获得缓存策略配置.
      *
-     * @return int
+     * @return array
+     * @throws
      */
-    protected function getCachePolicy()
+    protected function getCachePolicy(): array
     {
         $defaults = [
             'insert' => false,
@@ -166,12 +171,13 @@ trait Hooks
      * return record from cache if cache is created, or save data into cache.
      *
      * @param array        $id
-     * @param \Owl\Service $service
+     * @param Service|null $service
      * @param string       $collection
      *
-     * @return array
+     * @return array|bool
+     * @throws
      */
-    protected function doFind(array $id, \Owl\Service $service = null, $collection = null)
+    protected function doFind(array $id, Service $service = null, $collection = null)
     {
         if ($record = $this->getCache($id)) {
             return isset($record['__IS_NOT_FOUND__']) ? false : $record;
@@ -209,7 +215,7 @@ trait Hooks
      *
      * @return string
      */
-    protected function getCacheKey(array $id)
+    protected function getCacheKey(array $id): string
     {
         $prefix = $this->hasOption('cache_key')
         ? $this->getOption('cache_key')
