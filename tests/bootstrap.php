311c77835f628c90fe11244a80d1b08b3c936d19
hsldymq <hsldymq@qq.com>
2022-04-26 01:05:34 +0800

    fix: 跑数据库测试未生效

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,11 +14,3 @@ require __DIR__ . '/../vendor/autoload.php';
         'class' => \Tests\ORM\Mock\DataMapper\Service::class,
     ],
 ]);
-
-foreach (getenv() as $name => $value) {
-    if (str_starts_with($name, "OWL_TEST_")) {
-        $n = substr($name, strlen("OWL_TEST_"));
-        echo $n.': '.$value;
-        $_ENV[$n] = $value;
-    }
-}
